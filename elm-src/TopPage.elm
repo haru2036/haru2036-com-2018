63dd8d9d71450fdf1ce10d7f7f474809d26c1ae6
haru2036 <haru2036@gmail.com>
2018-01-31 08:55:18 +0900

    add some layouts

--- elm-src/TopPage.elm
+++ elm-src/TopPage.elm
@@ -1,6 +1,7 @@
 module FrontPage exposing (..)
 
 import Generated.API exposing (Page)
+import Css exposing(..)
 import Html 
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (class,id, css, href, src)
@@ -16,19 +17,74 @@ main =
 
 type Msg = NoOp
 
-update : msg -> Page -> Page 
+update : msg -> Model -> Model
 update _ _ = initialModel
 
-type alias Model = Page
+type Model = MainPage Page 
 
-initialModel : Page
-initialModel = Page "Hello, World!" "hello"
+initialModel : Model
+initialModel = MainPage <| Page "Hello, World!" "hello"
+
+mainPage : Page -> Html msg
+mainPage page = div [ class "main-wrapper"
+                 , css [ position relative 
+                       , width (pct 100)
+                       , height (pct 100)
+                       , backgroundImage (url "")
+                       , display block
+                       , backgroundSize cover
+                       , backgroundRepeat noRepeat
+                       , backgroundPosition center 
+                       ]
+                 ]
+                 [ div [ class "main-card" 
+                       , css 
+                         [ display inlineBlock
+                         , backgroundColor (rgb 250 250 250)
+                         , width (vw 70)
+                         , height (pct 40)
+                         , top (pct 0)
+                         , bottom (px 0)
+                         , right (pct 50)
+                         , left (pct 0)
+                         , margin4 auto auto auto auto
+                         , position absolute
+                         , boxShadow5 (px 0) (px 3) (px 5) (px 0) (rgba 0 0 0 0.5)
+                         , borderRadius (px 3) 
+                         , overflow hidden
+                         ]
+                       ]
+                       [ div [ css [
+                                     transform <| skewX <| deg -45
+                                   , overflow hidden
+                                   , height (pct 50)
+                                   , width(vh 40)
+                                   ]
+                             ]
+                             [ 
+                               img [ src ""
+                                   , css [ 
+                                           margin4 (px 0) (pct 50) (px 0) (px 0)
+                                         , transform <| skewX <| deg 45
+                                         , height (pct 100)
+                                         ] 
+                                   ] []
+                             ]
+                       , div [
+                         css [position relative 
+                             , width (pct 100)
+                             , height (pct 100)
+                             ]
+                         ] [
+                           h1 [] [ text page.pageTitle ]
+                         , section
+                           [ class "" ]
+                           [ text page.pageBody ]
+                         ]
+                       ]
+                 ]
+         
 
 view : Model -> Html msg
-view model = div [ class "main-wrapper"] 
-        [ h1 [] [ text model.pageTitle ]
-        , section
-          [ class "" ]
-          [ text model.pageBody ]
-        ]
-        
+view model = case model of
+  MainPage page -> mainPage page
