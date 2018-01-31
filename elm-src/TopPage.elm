70d84ba3d492ec289227494753ca103b6e510d63
haru2036 <haru2036@gmail.com>
2018-01-31 09:32:56 +0900

    separate page wrapper and body

--- elm-src/TopPage.elm
+++ elm-src/TopPage.elm
@@ -2,6 +2,8 @@ module FrontPage exposing (..)
 
 import Generated.API exposing (Page)
 import Css exposing(..)
+--import Css.Transitions exposing(height3, width3)
+import Time exposing(millisecond)
 import Html 
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (class,id, css, href, src)
@@ -25,8 +27,8 @@ type Model = MainPage Page
 initialModel : Model
 initialModel = MainPage <| Page "Hello, World!" "hello"
 
-mainPage : Page -> Html msg
-mainPage page = div [ class "main-wrapper"
+pageWrapper : Html msg -> Html msg
+pageWrapper body = div [ class "main-wrapper"
                  , css [ position relative 
                        , width (pct 100)
                        , height (pct 100)
@@ -37,7 +39,32 @@ mainPage page = div [ class "main-wrapper"
                        , backgroundPosition center 
                        ]
                  ]
-                 [ div [ class "main-card" 
+                 [ body ]
+                                 
+
+loadingPage : Html msg
+loadingPage = pageWrapper <| div [ class "main-card" 
+                       , css 
+                         [ display inlineBlock
+                         , backgroundColor (rgb 250 250 250)
+                         , height (pct 95)
+                         , width (pct 95)
+                         , top (pct 0)
+                         , bottom (px 0)
+                         , right (pct 50)
+                         , left (pct 0)
+                         , margin4 auto auto auto auto
+                         , position absolute
+                         , boxShadow5 (px 0) (px 3) (px 5) (px 0) (rgba 0 0 0 0.5)
+                         , borderRadius (px 3) 
+                         , overflow hidden
+                         ]
+                       ]
+                       [
+                       ]
+
+mainPage : Page -> Html msg
+mainPage page =  pageWrapper <| div [ class "main-card" 
                        , css 
                          [ display inlineBlock
                          , backgroundColor (rgb 250 250 250)
@@ -54,22 +81,25 @@ mainPage page = div [ class "main-wrapper"
                          , overflow hidden
                          ]
                        ]
-                       [ div [ css [
-                                     transform <| skewX <| deg -45
-                                   , overflow hidden
-                                   , height (pct 50)
-                                   , width(vh 40)
-                                   ]
-                             ]
-                             [ 
-                               img [ src ""
-                                   , css [ 
-                                           margin4 (px 0) (pct 50) (px 0) (px 0)
-                                         , transform <| skewX <| deg 45
-                                         , height (pct 100)
-                                         ] 
-                                   ] []
-                             ]
+
+                       [ div 
+                         [ css 
+                           [
+                             transform <| skewX <| deg -45
+                           , overflow hidden
+                           , height (pct 50)
+                           , width(vh 40)
+                           ]
+                         ]
+                         [ 
+                           img [ src ""
+                         , css [ 
+                                 margin4 (px 0) (pct 50) (px 0) (px 0)
+                               , transform <| skewX <| deg 45
+                               , height (pct 100)
+                               ] 
+                             ] []
+                         ]
                        , div [
                          css [position relative 
                              , width (pct 100)
@@ -80,9 +110,8 @@ mainPage page = div [ class "main-wrapper"
                          , section
                            [ class "" ]
                            [ text page.pageBody ]
-                         ]
                        ]
-                 ]
+                     ]
          
 
 view : Model -> Html msg
